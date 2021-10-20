6e54d746ec4903329bf2e4f236e56ba91fa04319
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2021-10-20 15:56:02 -0700

    ability to edit selected task

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,53 @@ var loadDate = function() {
 // time slot setup
 var loadTimeSlot = function() {
     var container = $(".container");
-    var timeSlot = $("<section>")
-        .addClass("row br-primary text-white")
-    var time = $("<p>")
-        .addClass("m-0 col-1 border-top border-bottom border-dark text-dark")
+
+    var timeList = $("<ul>")
+
+    var timeSlot = $("<li>")
+        .addClass("row")
+    
+    var time = $("<span>")
+        .addClass("m-0 col-1 border-top border-bottom border-dark")
         .text(moment().format("ha"));
+    
     var task = $("<p>")
         .addClass("m-0 col-10 bg-secondary border border-light")
+        .text("hello");
+    
     var save = $("<button>")
-        .addClass("m-0 col-1 bg-info border border-light rounded-right")
+        .addClass("m-0 col-1 bg-info border border-light rounded-right");
 
     timeSlot.append(time, task, save);
+    timeList.append(timeSlot);
     container.append(timeSlot);
 }
+
+// add and edit tasks
+$(".container").on("click", "p", function() {
+    var text = $(this)
+        .text()
+        .trim();
+
+    var textInput = $("<textarea>")
+        .addClass("form-control col-10 isSelected")
+        .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+ $(".container").on("click", "button", function() {
+    var text = $(".isSelected")
+        .val()
+        .trim();
+    
+    var textInput = $("<p>")
+        .addClass("m-0 col-10 bg-secondary border border-light")
+        .text(text);
+    
+    $(".isSelected").replaceWith(textInput);
+});
 
 loadDate();
 loadTimeSlot();
